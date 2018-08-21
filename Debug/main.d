6cf254dfd7610a7e72556c239b5d8273ab6ff937
Voyo <voyo@conserit.pl>
2018-08-21 22:35:23 +0200

    added as CCS project

---
+++ Debug/main.d
@@ -0,0 +1,25 @@
+# FIXED
+
+main.obj: ../main.c
+main.obj: C:/ti/ccs810/ccsv8/ccs_base/msp430/include/msp430.h
+main.obj: C:/ti/ccs810/ccsv8/ccs_base/msp430/include/msp430fr2311.h
+main.obj: C:/ti/ccs810/ccsv8/ccs_base/msp430/include/in430.h
+main.obj: C:/ti/ccs810/ccsv8/tools/compiler/ti-cgt-msp430_18.1.3.LTS/include/intrinsics.h
+main.obj: C:/ti/ccs810/ccsv8/tools/compiler/ti-cgt-msp430_18.1.3.LTS/include/intrinsics_legacy_undefs.h
+main.obj: C:/ti/ccs810/ccsv8/ccs_base/msp430/include/legacy.h
+main.obj: C:/ti/ccs810/ccsv8/tools/compiler/ti-cgt-msp430_18.1.3.LTS/include/string.h
+main.obj: C:/ti/ccs810/ccsv8/tools/compiler/ti-cgt-msp430_18.1.3.LTS/include/_ti_config.h
+main.obj: C:/ti/ccs810/ccsv8/tools/compiler/ti-cgt-msp430_18.1.3.LTS/include/linkage.h
+main.obj: C:/ti/ccs810/ccsv8/tools/compiler/ti-cgt-msp430_18.1.3.LTS/include/sys/cdefs.h
+
+../main.c: 
+C:/ti/ccs810/ccsv8/ccs_base/msp430/include/msp430.h: 
+C:/ti/ccs810/ccsv8/ccs_base/msp430/include/msp430fr2311.h: 
+C:/ti/ccs810/ccsv8/ccs_base/msp430/include/in430.h: 
+C:/ti/ccs810/ccsv8/tools/compiler/ti-cgt-msp430_18.1.3.LTS/include/intrinsics.h: 
+C:/ti/ccs810/ccsv8/tools/compiler/ti-cgt-msp430_18.1.3.LTS/include/intrinsics_legacy_undefs.h: 
+C:/ti/ccs810/ccsv8/ccs_base/msp430/include/legacy.h: 
+C:/ti/ccs810/ccsv8/tools/compiler/ti-cgt-msp430_18.1.3.LTS/include/string.h: 
+C:/ti/ccs810/ccsv8/tools/compiler/ti-cgt-msp430_18.1.3.LTS/include/_ti_config.h: 
+C:/ti/ccs810/ccsv8/tools/compiler/ti-cgt-msp430_18.1.3.LTS/include/linkage.h: 
+C:/ti/ccs810/ccsv8/tools/compiler/ti-cgt-msp430_18.1.3.LTS/include/sys/cdefs.h: 
